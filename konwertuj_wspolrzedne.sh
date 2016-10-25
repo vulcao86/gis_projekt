1818cad88cfb180ecee270015de525fcc6a42942
vulcao86 <piotrslupczewski@gmail.com>
2016-10-25 23:56:10 +0200

    dodaje duzo lokalizacji spoza trojmiasta

--- konwertuj_wspolrzedne.sh
+++ konwertuj_wspolrzedne.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # skrypt do konwersji wsplorzednych z urla do postaci akceptowalnej przez googlemaps api
 
-sed -e 's/.*@//' ./POI/miejsca_dzieci.csv | cut -d "," -f1,2 | sed 's/^/{lat: /' | sed 's/$/}/' | sed 's/,/, lng: /' >> ./POI/miejsca_dzieci_gotowe.csv
+sed -e 's/.*@//' ./POI/miejsca_poza_trojmiastem.csv | cut -d "," -f1,2 | sed 's/^/{lat: /' | sed 's/$/}/' | sed 's/,/, lng: /' >> ./POI/miejsca_poza_trojmiastem_gotowe.csv
