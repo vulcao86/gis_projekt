96d85bc6e43a7ce00ff8d901b5c61a99c6386acf
vulcao86 <piotrslupczewski@gmail.com>
2016-10-25 21:44:18 +0200

    dodaje nowa kategorie, folder POI, mod konwertera

--- konwertuj_wspolrzedne.sh
+++ konwertuj_wspolrzedne.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # skrypt do konwersji wsplorzednych z urla do postaci akceptowalnej przez googlemaps api
 
-sed -e 's/.*@//' miejsca.csv | cut -d "," -f1,2 | sed 's/^/{lat: /' | sed 's/$/}/' | sed 's/,/, lng: /' >> miejsca_gotowe.csv
+sed -e 's/.*@//' ./POI/miejsca_dzieci.csv | cut -d "," -f1,2 | sed 's/^/{lat: /' | sed 's/$/}/' | sed 's/,/, lng: /' >> ./POI/miejsca_dzieci_gotowe.csv
