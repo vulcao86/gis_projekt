1a504ce2dcf24976d3b74bf2fcb236e4f31e50e0
vulcao86 <piotrslupczewski@gmail.com>
2016-10-25 03:54:36 +0200

    dodaje nowerter + initial csv z danymi

---
+++ konwertuj_wspolrzedne.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+# skrypt do konwersji wsplorzednych z urla do postaci akceptowalnej przez googlemaps api
+
+sed -e 's/.*@//' miejsca.csv | cut -d "," -f1,2 | sed 's/^/{lat: /' | sed 's/$/}/' | sed 's/,/, lng: /'
+
