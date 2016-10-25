353c093ffc38bcc456370f135b1345f9cf1b8dc6
vulcao86 <piotrslupczewski@gmail.com>
2016-10-25 19:25:53 +0200

    dodalem molo w brzeznie

--- konwertuj_wspolrzedne.sh
+++ konwertuj_wspolrzedne.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # skrypt do konwersji wsplorzednych z urla do postaci akceptowalnej przez googlemaps api
 
-sed -e 's/.*@//' miejsca.csv | cut -d "," -f1,2 | sed 's/^/{lat: /' | sed 's/$/}/' | sed 's/,/, lng: /' >> miejsca_gotowe.csv
+sed -e 's/.*@//' miejsca_gdynia.csv.txt | cut -d "," -f1,2 | sed 's/^/{lat: /' | sed 's/$/}/' | sed 's/,/, lng: /' >> miejsca_gotowe.csv
