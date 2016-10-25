554f8b03489d92ecc1f8ff6eba4a3121eb789de4
vulcao86 <piotrslupczewski@gmail.com>
2016-10-25 04:01:55 +0200

    dodaje POI uzyskany konwerterem

--- konwertuj_wspolrzedne.sh
+++ konwertuj_wspolrzedne.sh
@@ -1,5 +1,5 @@
-#!bin/bash
+#!/bin/bash
 # skrypt do konwersji wsplorzednych z urla do postaci akceptowalnej przez googlemaps api
 
-sed -e 's/.*@//' miejsca.csv | cut -d "," -f1,2 | sed 's/^/{lat: /' | sed 's/$/}/' | sed 's/,/, lng: /'
+sed -e 's/.*@//' miejsca.csv | cut -d "," -f1,2 | sed 's/^/{lat: /' | sed 's/$/}/' | sed 's/,/, lng: /' >> miejsca_gotowe.csv
 
