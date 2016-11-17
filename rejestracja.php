549a7a4c8565483e9639d46cbdb315ab8085ba72
vulcao86 <vulcao86@users.noreply.github.com>
2016-11-17 22:50:00 +0100

    php register/login files

---
+++ rejestracja.php
@@ -0,0 +1,18 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>dane logowania</title>
+</head>
+<body>
+<?php
+$imie = $_GET['imie'];
+$nazwisko = $_GET['nazwisko'];
+echo 'Witaj!';
+echo $imie;
+echo "Oto Twoje dane:";
+echo("Twoje imię to $imie <br>");
+echo("Twoje nazwisko to $nazwisko <br>");
+?>
+</body>
+</html>
